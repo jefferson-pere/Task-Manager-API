1bc25d77d9377a8fb5af01946648d6b9ba222bfd
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-03-11 20:15:25 -0300

    add database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { routes } from "./routes";
 import { AppErrors } from "./errors/appErrors";
+import { sqliteConnection } from "./databases";
+import { runMigrations } from "./databases/migrations";
 
 const app = express();
 const PORT = process.env.PORT || 3333;
@@ -8,8 +10,24 @@ const PORT = process.env.PORT || 3333;
 app.use(express.json());
 app.use(routes);
 
-app.use(AppErrors)
+app.use(AppErrors);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}...`);
 });
+
+sqliteConnection()
+  .then(() => {
+    console.log("Database is connected!");
+  })
+  .catch((error) => {
+    console.log("Database ERROR - ", error);
+  });
+
+runMigrations()
+  .then(() => {
+    console.log("Migrations is connected!");
+  })
+  .catch((error) => {
+    console.log("Migrations ERROR - ", error);
+  });
