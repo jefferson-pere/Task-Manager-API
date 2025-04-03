8d3dd89215350141a86cf26a5c9ee7ba16511319
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-04-03 20:38:48 -0300

    add update

--- src/repositories/taskRepositoryInMemory.ts
+++ src/repositories/taskRepositoryInMemory.ts
@@ -74,9 +74,7 @@ export const taskRepositoryInMemory = {
       if (filter == "all") {
         return userTasks as CreateTaskDataTypes[];
       } else {
-        const filteredUserTasks = userTasks.filter(
-          (task) => task.status == filter
-        );
+        const filteredUserTasks = userTasks.filter((task) => task.status == filter);
         const paginatedTasks = filteredUserTasks.reverse();
         return paginatedTasks as CreateTaskDataTypes[];
       }
@@ -87,8 +85,7 @@ export const taskRepositoryInMemory = {
 
   async updateTask(data: UpdateTaskDataTypes) {
     try {
-      const { id, title, description, date, status, user_id, updated_at } =
-        data;
+      const { id, title, description, date, status, user_id, updated_at } = data;
 
       if (status != "completed" && status != "pending") {
         throw new Error("status must be 'completed' or 'pending'!");
