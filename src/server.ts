2c32648bf2259183f1f2d0e21bf7b59e8c5d8476
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-04-07 19:26:18 -0300

    update white list.

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-const whiteList = [
-  // "http://localhost:5173",
-  // "http://127.0.0.1:5173",
-  "https://task-manager-api-zs6u.onrender.com",
-];
+const whiteList = ["http://localhost:5173", "http://127.0.0.1:5173", "deploy"];
 
 app.use(
   cors({
