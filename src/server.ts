0949070e692c161f783dbd616ebc763cf3861b7c
Jefferson Pereira <jefferson.ps@hotmail.com.br>
2025-04-30 20:12:35 -0300

    add remove / w list

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 const whiteList = [
   // "http://localhost:5173",
   // "http://127.0.0.1:5173",
-  "https://task-manager-rho-neon.vercel.app/",
+  "https://task-manager-rho-neon.vercel.app",
 ];
 
 app.use(
